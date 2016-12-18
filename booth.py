851fb630260b4283f2110716751e61c77e23447a
Benoit Caron <bencaron@gmail.com>
2016-12-17 20:51:16 -0500

    First somewhat working version

--- booth.py
+++ booth.py
@@ -10,52 +10,57 @@
 # - on switch Off:
 #       shutdown the Rapspi
 
-import os
+import os, time
 from gpiozero import LED, Button
-from time import sleep
 from picamera import PiCamera
 from signal import pause
 
 save_path = os.path.join(os.environ['HOME'], 'photopi')
-blink_time = 0.2
-pin_green_led = 7
-pin_red_led = 11
-pin_photo = 15
+blink_time = 1
+pin_green_led = 4
+pin_red_led = 17
+pin_photo = 22
 #pin_off = 24
 nb_pictures = 4
 
 
 def takePictures(camera, batch):
+    print "taking picture for batch %i" % batch
     for x in range(0, nb_pictures):
         camera.capture(os.path.join(save_path, "photo_%s_%s.jpg" % (batch, x)))
 
 def pictureButtonPress(camera, green, red):
+    print "in pictureButtonPress"
     batch = time.time()
-    green.off
-    red.blink(blink_time, blink_time, 5, False)
+    green.off()
+    red.blink(blink_time, blink_time, blink_time * nb_pictures, True)
     takePictures(camera, batch)
-    red.blink(1, blink_time, 1, False)
-    green.on
+    red.off()
+    green.on()
 
 
 if __name__ == '__main__':
     print "hello"
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
 
     greenLed = LED(pin_green_led)
     redLed = LED(pin_red_led)
     button = Button(pin_photo)
 
-    redLed.on
-    greenLed.blink(blink_time, blink_time)
+    redLed.on()
+    greenLed.off()
 
     camera = PiCamera()
     camera.resolution = (1024, 768)
     camera.start_preview()
+    greenLed.on()
 
-    greenLed.on
-    redLed.off
-
-    button.when_pressed = pictureButtonPress(camera, greenLed, redLed)
+    while True:
+        print "Waiting for button...."
+        greenLed.on()
+        button.wait_for_press()
+        pictureButtonPress(camera, greenLed, redLed)
 
     # loop
-    pause()
+    #pause()
