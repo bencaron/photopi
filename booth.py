a869e675b20172e650a68e0acbaab064e96266e0
Benoit Caron <bencaron@gmail.com>
2016-12-22 22:20:53 -0500

    Changing LED pinout + playing with the led toogling

--- booth.py
+++ booth.py
@@ -17,8 +17,8 @@ from signal import pause
 
 save_path = os.path.join(os.environ['HOME'], 'photopi')
 time_blink = 1
-time_between_pics = 5
-pin_green_led = 4
+time_between_pics = 3
+pin_green_led = 27
 pin_red_led = 17
 pin_photo = 22
 #pin_off = 24
@@ -27,24 +27,23 @@ nb_pictures = 4
 
 def takePictures(camera, batch, led):
     print "taking picture for batch %i" % batch
-    #led.blink(time_blink, time_blink)
     for x in range(0, nb_pictures):
-        led.on()
+        led.toggle()
         camera.capture(os.path.join(save_path, "photo_%s_%s.jpg" % (batch, x)))
+        led.toggle()
         time.sleep(time_between_pics)
-        led.off()
-    #led.off()
 
 def pictureButtonPress(camera, green, red):
     print "in pictureButtonPress"
     batch = time.time()
     green.off()
-    takePictures(camera, batch, red)
+    red.on()
+    takePictures(camera, batch, green)
+    red.off()
     green.on()
 
 
 if __name__ == '__main__':
-    print "hello"
     if not os.path.exists(save_path):
         os.mkdir(save_path)
 
@@ -56,14 +55,17 @@ if __name__ == '__main__':
     greenLed.off()
 
     camera = PiCamera()
-    camera.resolution = (1024, 768)
+    #camera.resolution = (1024, 768)
+    camera.resolution = (2592, 1944)
     camera.start_preview()
     greenLed.on()
     redLed.off()
 
-    while True:
+    #while True:
+    for x in range(0,2):
         print "Waiting for button...."
         button.wait_for_press()
+        #time.sleep(10)
         pictureButtonPress(camera, greenLed, redLed)
 
     # loop
